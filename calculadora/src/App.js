d28cf3970a2d576f06710aa35681cfe3288d440e
Danilo Santos <daanilocarvalho712@gmail.com>
2022-12-12 20:21:46 -0300

    primeiro commit

---
+++ calculadora/src/App.js
@@ -0,0 +1,123 @@
+import Input from './components/Input'
+import Button from './components/button'
+import { useState } from 'react'
+
+import {Container, Content, Row } from './styles/styles'
+
+const App = () => {
+  const [currentNumber, setCurrentNumber] = useState('0');
+  const [firstNumber, setfirstNumber] = useState();
+  const [operation, setOperation] = useState();
+
+  const handleOnClear = () => {
+    setCurrentNumber('0');
+    setfirstNumber('0');
+    setOperation('');
+  }
+
+  const handleAddNumber = (num)=> {
+    setCurrentNumber(prev => `${prev === '0' ? '' : prev }${num}`)
+  }
+
+  const handleSumNumbers = () => {
+    if(firstNumber === '0'){
+      setfirstNumber(String(currentNumber));
+      setCurrentNumber('0')
+      setOperation('+')
+    }else{
+      const sum = Number(firstNumber) + Number(currentNumber);
+      setCurrentNumber(String(sum));
+      setOperation('')
+      
+  }
+}
+const handleMinusNumbers = () => {
+  if(firstNumber === '0'){
+    setfirstNumber(String(currentNumber));
+    setCurrentNumber('0')
+    setOperation('-')
+  }else{
+    const minus = Number(firstNumber) - Number(currentNumber);
+    setCurrentNumber(String(minus));
+    setOperation('')
+    
+  }
+}
+const handleMultiplyNumbers = () => {
+  if(firstNumber === '0'){
+    setfirstNumber(String(currentNumber));
+    setCurrentNumber('0')
+    setOperation('x')
+  }else{
+    const multiply = Number(firstNumber) * Number(currentNumber);
+    setCurrentNumber(String(multiply));
+    setOperation('')
+    
+  }
+}
+const handleDivisionNumbers = () => {
+  if(firstNumber === '0'){
+    setfirstNumber(String(currentNumber));
+    setCurrentNumber('0')
+    setOperation('/')
+  }else{
+    const division = Number(firstNumber) / Number(currentNumber);
+    setCurrentNumber(String(division));
+    setOperation('')
+  }
+}
+
+const handleEquals = () => {
+  if(firstNumber !== '0' && operation !== '' && currentNumber !== '0'){
+    switch(operation){
+      case '+':
+        handleSumNumbers();
+        break;
+        case '-':
+        handleMinusNumbers();
+        break;
+        case 'x':
+        handleMultiplyNumbers();
+        break;
+        case '/':
+        handleDivisionNumbers();
+        break;
+        default:
+          break;
+    }
+  }
+}
+  return (
+    <Container>
+      <Content>
+        <Input value={currentNumber}/>
+        <Row>
+          <Button label="." onClick={() => handleAddNumber('.')}/>
+          <Button label="/" onClick={() => handleDivisionNumbers()}/>
+          <Button label="C" onClick={() => handleOnClear()}/>
+          <Button label="x" onClick={() => handleMultiplyNumbers()}/>
+        </Row>
+        <Row>
+          <Button label="7"onClick={() => handleAddNumber('7')}/>
+          <Button label="8"onClick={() => handleAddNumber('8')}/>
+          <Button label="9"onClick={() => handleAddNumber('9')}/>
+          <Button label="-"onClick={() => handleMinusNumbers()}/>
+        </Row>
+        <Row>
+          <Button label="4"onClick={() => handleAddNumber('4')}/>
+          <Button label="5"onClick={() => handleAddNumber('5')}/>
+          <Button label="6"onClick={() => handleAddNumber('6')}/>
+          <Button label="+"onClick={() => handleSumNumbers()}/>
+        </Row>
+        <Row>
+          <Button label="1" onClick={() => handleAddNumber('1')}/>
+          <Button label="2" onClick={() => handleAddNumber('2')}/>
+          <Button label="3" onClick={() => handleAddNumber('3')}/>
+          <Button label="=" onClick={() => handleEquals()}/>
+        </Row>   
+      </Content>
+    </Container>
+  );
+}
+
+export default App;
---
+++ calculadora/src/components/button/styles.js
@@ -0,0 +1,18 @@
+import  styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding:10px;
+    border:1px solid #CDCDCD;
+    background-color:#00AAF0;
+    color:#FFFFFF;
+    font-size:24px;
+    font-weight:700; 
+    flex:1;
+    height:70px;
+    border-radius:10px;
+
+    :hover{
+        opacity:0.6;
+    }
+
+`
